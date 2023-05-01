3f079ba1ab3d9f953860c087ed04faab6f8d74d9
Rafael-Danstim <rafaeldanstim15@gmail.com>
2023-05-01 10:36:23 -0300

    01/04/2023 - Início da outra tela criado!

--- app/src/main/java/com/example/localmap/MainActivity.java
+++ app/src/main/java/com/example/localmap/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.localmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button botaoLogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoLogar = findViewById(R.id.buttonLogar);
+
+        botaoLogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainHome = new Intent(getApplicationContext(),MainHome.class);
+                startActivity(mainHome);
+            }
+        });
     }
 }
