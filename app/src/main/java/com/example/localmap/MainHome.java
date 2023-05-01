426f7ede110ad669519107cc47b4419afd2578b6
Rafael-Danstim <rafaeldanstim15@gmail.com>
2023-05-01 16:14:57 -0300

    01/05/2023 - Adição de uma ScrollView vertical na tela!

--- app/src/main/java/com/example/localmap/MainHome.java
+++ app/src/main/java/com/example/localmap/MainHome.java
@@ -3,6 +3,14 @@ package com.example.localmap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainHome extends AppCompatActivity {
 
