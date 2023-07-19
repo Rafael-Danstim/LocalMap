65ab0a1bf55d8682c45e4cf3cd0ec0bd6442d7f0
Rafael-Danstim <rafaeldanstim15@gmail.com>
2023-07-19 17:24:17 -0300

    19/07/2023 - De volta a estaca zero (missão API Google)!

--- app/src/main/java/com/example/localmap/activities/MainActivity.java
+++ app/src/main/java/com/example/localmap/activities/MainActivity.java
@@ -6,20 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.example.localmap.R;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
+import com.example.localmap.ui.home.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    GoogleSignInOptions googleSignInOptions;
-    GoogleSignInClient googleSignInClient;
     private Button botaoLogar;
 
     @Override
@@ -27,45 +19,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
         botaoLogar = findViewById(R.id.buttonLogar);
-
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if(account != null){
-            navegarParaMainInicio();
-        }
 
         botaoLogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                signIn();
-
+                Intent i1 = new Intent(getApplicationContext(), InicioActivity.class);
+                startActivity(i1);
             }
         });
-    }
-    void signIn(){
-        Intent signInIntent = googleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, 1000);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1000){
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-
-            try {
-                task.getResult(ApiException.class);
-                navegarParaMainInicio();
-            } catch (ApiException e) {
-                Toast.makeText(this, "Algo deu errado!", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-    void navegarParaMainInicio(){
-        finish();
-        Intent mainInicio = new Intent(getApplicationContext(), InicioActivity.class);
-        startActivity(mainInicio);
     }
 }
