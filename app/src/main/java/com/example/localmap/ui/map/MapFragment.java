23fd0f163cabfcbd1ca5656b1469fc569f3f20a9
slaye <slayerdtempest@gmail.com>
2023-08-31 07:32:06 -0300

    31/08/2023 - Mapa adicionado, mas não abre os estabelecimentos!

--- app/src/main/java/com/example/localmap/ui/map/MapFragment.java
+++ app/src/main/java/com/example/localmap/ui/map/MapFragment.java
@@ -1,19 +1,23 @@
 package com.example.localmap.ui.map;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.localmap.databinding.FragmentMapBinding;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;;
+import com.google.android.gms.maps.model.LatLng;
 
-public class MapFragment extends Fragment {
+
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
     private FragmentMapBinding binding;
+    private MapView mapView;
+    private GoogleMap googleMap;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -22,6 +26,11 @@ public class MapFragment extends Fragment {
         binding = FragmentMapBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // Pega o id mapView da fragment_map;
+        mapView = binding.mapView;
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
         return root;
     }
 
@@ -29,5 +38,39 @@ public class MapFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+
+        LatLng SantaLuzia = new LatLng(-6.875794544797201, -36.91962465083226); // Define a cidade do mapa para Santa Luzia.
+        float zoomLevel = 17.0f; // Define o nível de zoom desejado para a exibição no mapa.
+
+        googleMap.moveCamera(com.google.android.gms.maps.CameraUpdateFactory.newLatLngZoom(SantaLuzia, zoomLevel));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
